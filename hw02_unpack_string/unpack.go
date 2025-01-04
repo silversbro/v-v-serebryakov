7c095a51c8aa079127abcba4f57f72764cea1d5d
Slava Silver <serebryakov@bgit.ru>
2025-01-04 11:29:54 +0300

    fix liter v2

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -10,6 +10,7 @@ import (
 var ErrInvalidString = errors.New("invalid string")
 
 func Unpack(str string) (string, error) {
+
 	// Преобразуем строку в срез рун
 	runes := []rune(str)
 	readyString, err := WriteString(runes, str)
@@ -25,6 +26,7 @@ func Unpack(str string) (string, error) {
 
 func WriteString(runes []rune, text string) (string, error) {
 	var builder strings.Builder
+
 	// Распечатаем каждую руну отдельно
 	for i, r := range runes {
 		num, err := strconv.Atoi(string(r))
@@ -41,11 +43,13 @@ func WriteString(runes []rune, text string) (string, error) {
 				builder.WriteString(string(r))
 				index := strings.Index(builder.String(), "0")
 				revertVal := builder.String()[:index-1]
+
 				builder.Reset()
 				builder.WriteString(revertVal)
 
 				continue
 			}
+
 			repeated := strings.Repeat(string(runes[i-1]), num-1)
 			builder.WriteString(repeated)
 		}
@@ -58,6 +62,7 @@ func CheckDecimal(num string, text string) bool {
 	var checkBuilder strings.Builder
 	checkBuilder.WriteString(num)
 	checkBuilder.WriteString("0")
+	index := strings.Index(text, checkBuilder.String())
 
-	return strings.Index(text, checkBuilder.String()) == -1
+	return index != -1
 }
