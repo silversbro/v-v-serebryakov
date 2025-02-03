030255378c757108ddc021abe842cdd1ea37e9cd
Slava Silver <serebryakov@bgit.ru>
2025-02-03 16:03:21 +0300

    fix review

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -14,12 +14,11 @@ func Top10(text string) []string {
 	words := strings.Fields(text)
 	countMap := make(map[string]int)
 	countWords := make([]CountWord, 0)
+	readySliceSize := 10
 
 	for _, word := range words {
 		countMap[word]++
 	}
-
-	readySlice := make([]string, len(countMap))
 
 	for i, countWord := range countMap {
 		el := CountWord{Word: i, Count: countWord}
@@ -34,8 +33,14 @@ func Top10(text string) []string {
 		return countWords[i].Count > countWords[j].Count
 	})
 
+	if readySliceSize > len(countWords) {
+		readySliceSize = len(countWords)
+	}
+
+	readySlice := make([]string, readySliceSize)
+
 	for i, countWord := range countWords {
-		readySlice = append(readySlice, countWord.Word)
+		readySlice[i] = countWord.Word
 		if i == 9 {
 			break
 		}
