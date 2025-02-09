6633f44aef91c9e4e9ea7f3158d44d9d38161492
Slava Silver <serebryakov@bgit.ru>
2025-02-09 16:07:08 +0300

    make list

--- hw04_lru_cache/list.go
+++ hw04_lru_cache/list.go
@@ -1,13 +1,13 @@
 package hw04lrucache
 
 type List interface {
-	Len() int
-	Front() *ListItem
-	Back() *ListItem
-	PushFront(v interface{}) *ListItem
-	PushBack(v interface{}) *ListItem
-	Remove(i *ListItem)
-	MoveToFront(i *ListItem)
+	Len() int                          // Длина списка
+	Front() *ListItem                  // Первый элемент списка
+	Back() *ListItem                   // Последний элемент списка
+	PushFront(v interface{}) *ListItem // Добавление значения в начало
+	PushBack(v interface{}) *ListItem  // Добавление значения в конец
+	Remove(i *ListItem)                // Удаление элемента
+	MoveToFront(i *ListItem)           // Перемещение элемента в начало
 }
 
 type ListItem struct {
@@ -17,10 +17,96 @@ type ListItem struct {
 }
 
 type list struct {
-	List // Remove me after realization.
-	// Place your code here.
+	head *ListItem // Указатель на голову списка
+	tail *ListItem // Указатель на хвост списка
+	len  int       // Длина списка
 }
 
 func NewList() List {
-	return new(list)
+	return &list{
+		head: nil,
+		tail: nil,
+		len:  0,
+	}
+}
+
+func (list *list) Len() int {
+	return list.len
+}
+
+func (list *list) Front() *ListItem {
+	return list.head
+}
+
+func (list *list) Back() *ListItem {
+	return list.tail
+}
+
+func (list *list) PushFront(v interface{}) *ListItem {
+	newItem := &ListItem{
+		Value: v,
+		Next:  list.head,
+		Prev:  nil,
+	}
+
+	if list.head != nil {
+		list.head.Prev = newItem
+	}
+
+	list.head = newItem
+
+	if list.tail == nil {
+		list.tail = newItem
+	}
+
+	list.len++
+
+	return newItem
+}
+
+func (list *list) PushBack(v interface{}) *ListItem {
+	newItem := &ListItem{
+		Value: v,
+		Next:  nil,
+		Prev:  list.tail,
+	}
+
+	if list.tail != nil {
+		list.tail.Next = newItem
+	}
+
+	list.tail = newItem
+
+	if list.head == nil {
+		list.head = newItem
+	}
+
+	list.len++
+
+	return newItem
+}
+
+func (list *list) Remove(item *ListItem) {
+	if item.Prev != nil {
+		item.Prev.Next = item.Next
+	} else {
+		list.head = item.Next
+	}
+
+	if item.Next != nil {
+		item.Next.Prev = item.Prev
+	} else {
+		list.tail = item.Prev
+	}
+
+	list.len--
+}
+
+func (list *list) MoveToFront(item *ListItem) {
+	if item == list.head {
+		return
+	}
+
+	list.Remove(item)
+	list.PushFront(item.Value)
 }
