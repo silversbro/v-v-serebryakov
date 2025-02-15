124ee8a3fa42818c23205103aa6743b13f4bc691
Slava Silver <serebryakov@bgit.ru>
2025-02-15 17:00:07 +0300

    fix

--- hw04_lru_cache/cache_test.go
+++ hw04_lru_cache/cache_test.go
@@ -10,39 +10,35 @@ import (
 )
 
 func TestCache(t *testing.T) {
+	c := NewCache(5)
 
-	t.Run("simple", func(t *testing.T) {
-		c := NewCache(5)
+	wasInCache := c.Set("aaa", 100)
+	require.False(t, wasInCache)
 
-		wasInCache := c.Set("aaa", 100)
-		require.False(t, wasInCache)
+	wasInCache = c.Set("bbb", 200)
+	require.False(t, wasInCache)
 
-		wasInCache = c.Set("bbb", 200)
-		require.False(t, wasInCache)
+	val, ok := c.Get("aaa")
+	require.True(t, ok)
+	require.Equal(t, 100, val)
 
-		val, ok := c.Get("aaa")
-		require.True(t, ok)
-		require.Equal(t, 100, val)
+	val, ok = c.Get("bbb")
+	require.True(t, ok)
+	require.Equal(t, 200, val)
 
-		val, ok = c.Get("bbb")
-		require.True(t, ok)
-		require.Equal(t, 200, val)
+	wasInCache = c.Set("aaa", 300)
+	require.True(t, wasInCache)
 
-		wasInCache = c.Set("aaa", 300)
-		require.True(t, wasInCache)
+	val, ok = c.Get("aaa")
+	require.True(t, ok)
+	require.Equal(t, 300, val)
 
-		val, ok = c.Get("aaa")
-		require.True(t, ok)
-		require.Equal(t, 300, val)
-
-		val, ok = c.Get("ccc")
-		require.False(t, ok)
-		require.Nil(t, val)
-	})
+	val, ok = c.Get("ccc")
+	require.False(t, ok)
+	require.Nil(t, val)
 }
 
 func TestCacheClear(t *testing.T) {
-
 	c := NewCache(10)
 
 	c.Set("aaa", 100)
@@ -51,7 +47,6 @@ func TestCacheClear(t *testing.T) {
 
 	_, ok := c.Get("aaa")
 	require.False(t, ok)
-
 }
 
 func TestCachePure(t *testing.T) {
