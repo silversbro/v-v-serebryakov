30aa818042c1f2e9f80181bd891b1c69c511907e
Slava Silver <serebryakov@bgit.ru>
2025-03-02 13:49:09 +0300

    add code

--- hw05_parallel_execution/main.go
+++ hw05_parallel_execution/main.go
@@ -1,45 +1,121 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"math/rand"
+	"os"
+	"os/signal"
 	"sync"
+	"syscall"
+	"time"
 )
 
+var ErrErrorsLimitExceeded = errors.New("errors limit exceeded")
+
+type randNumSlice struct {
+	mu     sync.Mutex
+	slRand []int
+}
+
 func main() {
-	wg := sync.WaitGroup{}
+	//	1 - работа с каналом
+	//	2 - сделать структуру с мьютексом
+	//	3 - логи в do.once
+	//	4 - ассинхронность
+	//	5 - select для работы с каналами что бы не было вечного ожидания
+	//	6 - обработка ошибок
+
+	sliceMu := &randNumSlice{
+		slRand: make([]int, 0),
+	}
+
+	readySlice := make([]int, 0)
+
+	chData := make(chan int)
+	chErrorSignal := make(chan os.Signal, 1)
 	var once sync.Once
+
+	defer close(chData)
+	defer close(chErrorSignal)
+
+	wg := sync.WaitGroup{}
 
 	for i := 0; i < 150; i++ {
 		wg.Add(1) // <===
 		go func() {
 			defer wg.Done()
-			if mtRand(0, 150) == i {
-				once.Do(errorPrint)
-			}
-			fmt.Println("go-go-go from:", i)
+			addRandToSlice(chData, chErrorSignal, &once)
 		}()
 	}
 
-	for i := 0; i < 100; i++ {
-		wg.Add(1) // <===
-		go func() {
-			defer wg.Done()
-			if mtRand(0, 100) == i {
-				once.Do(errorPrint)
-			}
-			fmt.Println("go-go-go from:", i)
-		}()
-	}
+	wg.Add(1) // <===
+	go func() {
+		defer wg.Done()
+		readySlice = readFromChain(sliceMu, chData, chErrorSignal).slRand
+	}()
 
 	wg.Wait()
+
+	println(readySlice)
+}
+
+func addRandToSlice(input chan<- int, chErrorSignal chan os.Signal, once *sync.Once) {
+	start := mtRand(1, 5)
+	end := mtRand(1, 5)
+
+	if start == end {
+		errorSet(once, chErrorSignal)
+	}
+
+	select {
+	case <-chErrorSignal:
+
+	default:
+		input <- mtRand(1, 100)
+		return
+	}
+}
+
+func errorSet(once *sync.Once, chErrorSignal chan os.Signal) {
+	once.Do(func() {
+		signal.Notify(chErrorSignal, syscall.SIGINT, syscall.SIGTERM)
+	})
+}
+
+func readFromChain(sliceMu *randNumSlice, input <-chan int, chErrorSignal <-chan os.Signal) randNumSlice {
+
+	// Таймер на 4 секунды
+	timer := time.NewTimer(4 * time.Second)
+	defer timer.Stop()
+
+	for {
+		x, ok := <-input
+		if !ok {
+			break
+		}
+
+		select {
+		case <-chErrorSignal:
+			fmt.Printf("Error")
+			break
+		case <-timer.C:
+			fmt.Println("End.")
+			break
+		default:
+			sliceMu.mu.Lock()
+			sliceMu.slRand = append(sliceMu.slRand, x)
+			sliceMu.mu.Unlock()
+		}
+	}
+
+	return randNumSlice{slRand: sliceMu.slRand}
 }
 
 func errorPrint() {
 	fmt.Println("Error One time")
 }
 
-// mtRand имитирует функцию mt_rand из PHP
 func mtRand(min, max int) int {
 	if min > max {
 		min, max = max, min
