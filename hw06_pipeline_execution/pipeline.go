958d41d48c780d37b42f086619407f15ecfa1829
Slava Silver <serebryakov@bgit.ru>
2025-05-09 15:36:07 +0300

    fix pipline errors

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -11,76 +11,81 @@ type (
 type Stage func(in In) (out Out)
 
 func ExecutePipeline(in In, done In, stages ...Stage) Out {
-	// Если нет стейджей, просто возвращаем входной канал
 	if len(stages) == 0 {
 		return in
 	}
 
-	// Создаем выходной канал
 	out := make(chan interface{})
-
 	var wg sync.WaitGroup
 
-	// Запускаем обработку в отдельной горутине
-	go func() {
-		defer close(out)
+	stageChans := make([]chan interface{}, len(stages)+1)
+	for i := range stageChans {
+		stageChans[i] = make(chan interface{})
+	}
 
-		// Создаем каналы для каждого стейджа
-		stageChans := make([]chan interface{}, len(stages)+1)
-		for i := range stageChans {
-			stageChans[i] = make(chan interface{})
-		}
+	for i, stage := range stages {
+		wg.Add(1)
+		go func(index int, s Stage) {
+			defer wg.Done()
+			defer close(stageChans[index+1])
 
-		// Запускаем все стейджи в отдельных горутинах
-		for i, stage := range stages {
-			wg.Add(1)
-			go func(index int, s Stage) {
-				defer wg.Done()
-				defer close(stageChans[index+1])
-
-				// Запускаем стейдж и перенаправляем его вывод
-				for val := range s(stageChans[index]) {
-					select {
-					case <-done:
-						return
-					case stageChans[index+1] <- val:
-					}
-				}
-			}(i, stage)
-		}
-
-		// Перенаправляем входные данные в первый стейдж
-		go func() {
-			defer close(stageChans[0])
 			for {
 				select {
 				case <-done:
 					return
-				case val, ok := <-in:
+				case v, ok := <-stageChans[index]:
 					if !ok {
 						return
 					}
-					select {
-					case <-done:
-						return
-					case stageChans[0] <- val:
+					sOut := s(makeInChan(v))
+					for v2 := range sOut {
+						select {
+						case <-done:
+							return
+						case stageChans[index+1] <- v2:
+						}
 					}
 				}
 			}
-		}()
+		}(i, stage)
+	}
 
-		// Перенаправляем вывод последнего стейджа в выходной канал
-		for val := range stageChans[len(stageChans)-1] {
+	go func() {
+		defer close(stageChans[0])
+		for {
 			select {
 			case <-done:
 				return
-			case out <- val:
+			case v, ok := <-in:
+				if !ok {
+					return
+				}
+				select {
+				case <-done:
+					return
+				case stageChans[0] <- v:
+				}
 			}
 		}
+	}()
 
-		// Ждем завершения всех стейджей
-		wg.Wait()
+	go func() {
+		defer close(out)
+		for v := range stageChans[len(stageChans)-1] {
+			select {
+			case <-done:
+				return
+			case out <- v:
+			}
+		}
 	}()
 
 	return out
+}
+
+func makeInChan(v interface{}) In {
+	ch := make(chan interface{}, 1)
+	ch <- v
+	close(ch)
+	return ch
 }
